31628f669a48d1806b14aa1c63e65cf4a5305d4a
OneMeanDragon <dragonkings@gmail.com>
2021-10-29 11:03:35 -0400

    Add files via upload

---
+++ Graphics/Factorys/IWindowFactory.vb
@@ -0,0 +1,11 @@
+﻿
+Namespace Graphics
+
+    Interface IWindowFactory
+        Function CreateWindow(ByVal title As String) As IWindow
+        Function CreateWindow(ByVal title As String, ByVal x As Integer, ByVal y As Integer) As IWindow
+        Function CreateWindow(ByVal title As String, ByVal x As Integer, ByVal y As Integer, ByVal width As Integer, ByVal height As Integer) As IWindow
+        Function CreateWindow(ByVal title As String, ByVal x As Integer, ByVal y As Integer, ByVal width As Integer, ByVal height As Integer, ByVal flags As WindowFlags) As IWindow
+    End Interface
+
+End Namespace
---
+++ Graphics/Factorys/WindowFactory.vb
@@ -0,0 +1,39 @@
+﻿Imports System
+Imports Microsoft.Extensions.Logging
+
+Namespace Graphics
+    Public Class WindowFactory
+        Implements IWindowFactory
+
+        Private ReadOnly logger As ILogger(Of WindowFactory)
+        Private ReadOnly loggerWindow As ILogger(Of Window)
+
+        Public Sub New(ByVal Optional logger As ILogger(Of WindowFactory) = Nothing, ByVal Optional loggerWindow As ILogger(Of Window) = Nothing)
+            Me.logger = logger
+            Me.loggerWindow = loggerWindow
+        End Sub
+
+        Public Function CreateWindow(ByVal title As String) As IWindow Implements IWindowFactory.CreateWindow
+            Return CreateWindow(title, 100, 100, 1280, 720, WindowFlags.Shown)
+        End Function
+
+        Public Function CreateWindow(ByVal title As String, ByVal x As Integer, ByVal y As Integer) As IWindow Implements IWindowFactory.CreateWindow
+            Return CreateWindow(title, x, y, 1280, 720, WindowFlags.Shown)
+        End Function
+
+        Public Function CreateWindow(ByVal title As String, ByVal x As Integer, ByVal y As Integer, ByVal width As Integer, ByVal height As Integer) As IWindow Implements IWindowFactory.CreateWindow
+            Return CreateWindow(title, x, y, width, height, WindowFlags.Shown)
+        End Function
+
+        Public Function CreateWindow(ByVal title As String, ByVal x As Integer, ByVal y As Integer, ByVal width As Integer, ByVal height As Integer, ByVal flags As WindowFlags) As IWindow Implements IWindowFactory.CreateWindow
+            Try
+                Dim window = New Window(title, x, y, width, height, flags, loggerWindow)
+                logger?.LogTrace($"Window created. Title = {window.Title}, X = {window.X}, Y = {window.Y}, Width = {window.Width}, Height = {window.Height}, Handle = {window.Handle}.")
+                Return window
+            Catch ex As Exception
+                logger?.LogError(ex, ex.Message)
+                Throw
+            End Try
+        End Function
+    End Class
+End Namespace
