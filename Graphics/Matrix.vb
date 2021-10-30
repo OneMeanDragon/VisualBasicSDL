40ad288749b6f145f019b4872467c907f8767d40
OneMeanDragon <dragonkings@gmail.com>
2021-10-30 11:42:17 -0400

    Add files via upload

---
+++ Graphics/Matrix.vb
@@ -0,0 +1,84 @@
+﻿
+Namespace Graphics
+
+    Public Structure Matrix
+        Public Property Row1Col1 As Single
+        Public Property Row1Col2 As Single
+        Public Property Row2Col1 As Single
+        Public Property Row2Col2 As Single
+
+        Public Sub New(ByVal row1col1 As Single, ByVal row1col2 As Single, ByVal row2col1 As Single, ByVal row2col2 As Single)
+            Me.New()
+            row1col1 = row1col1
+            row1col2 = row1col2
+            row2col1 = row2col1
+            row2col2 = row2col2
+        End Sub
+
+        Default Public Property Item(ByVal index As Integer) As Single
+            Get
+
+                Select Case index
+                    Case 0
+                        Return Row1Col1
+                    Case 1
+                        Return Row1Col2
+                    Case 2
+                        Return Row2Col1
+                    Case 3
+                        Return Row2Col2
+                    Case Else
+                        Throw New ArgumentOutOfRangeException()
+                End Select
+            End Get
+            Set(ByVal value As Single)
+
+                Select Case index
+                    Case 0
+                        Row1Col1 = value
+                    Case 1
+                        Row1Col2 = value
+                    Case 2
+                        Row2Col1 = value
+                    Case 3
+                        Row2Col2 = value
+                    Case Else
+                        Throw New ArgumentOutOfRangeException()
+                End Select
+            End Set
+        End Property
+
+        Default Public Property Item(ByVal row As Integer, ByVal column As Integer) As Single
+            Get
+                Return Me(row * 2 + column)
+            End Get
+            Set(ByVal value As Single)
+                Me(row * 2 + column) = value
+            End Set
+        End Property
+
+        Public Shared Function Invert(ByVal matrix As Matrix) As Matrix
+            Dim determinant As Single = 1 / ((matrix.Row1Col1 * matrix.Row2Col2) - (matrix.Row1Col2 * matrix.Row2Col1))
+            Dim newRow1Col1 As Single = matrix.Row2Col2
+            Dim newRow1Col2 As Single = matrix.Row1Col2 * -1
+            Dim newRow2Col1 As Single = matrix.Row2Col1 * -1
+            Dim newRow2Col2 As Single = matrix.Row1Col1
+
+            newRow1Col1 /= determinant
+            newRow1Col2 /= determinant
+            newRow2Col1 /= determinant
+            newRow2Col2 /= determinant
+
+            Return New Matrix(newRow1Col1, newRow1Col2, newRow2Col1, newRow2Col2)
+        End Function
+
+        Public Shared Function CreateScale(ByVal scaleX As Single, ByVal scaleY As Single) As Matrix
+            Return New Matrix() With {
+                .Row1Col1 = scaleX,
+                .Row1Col2 = 0,
+                .Row2Col1 = 0,
+                .Row2Col2 = scaleY
+            }
+        End Function
+    End Structure
+End Namespace
---
+++ Graphics/Vector2D.vb
@@ -0,0 +1,78 @@
+﻿Imports System
+
+Namespace Graphics
+
+    Public Class Vector2D
+        Public Property X As Single
+        Public Property Y As Single
+
+        Public Shared ReadOnly Property One As Vector2D
+            Get
+                Return New Vector2D(1, 1)
+            End Get
+        End Property
+
+        Public Shared ReadOnly Property Zero As Vector2D
+            Get
+                Return New Vector2D(0, 0)
+            End Get
+        End Property
+
+        Public Sub New(ByVal x As Single, ByVal y As Single)
+            x = x
+            y = y
+        End Sub
+
+        Public Function Transform(ByVal matrix As Matrix) As Vector2D
+            Return New Vector2D((X * matrix.Row1Col1) + (Y * matrix.Row2Col1), (X * matrix.Row2Col1) + (Y * matrix.Row2Col2))
+        End Function
+
+        Public Function Add(ByVal vector As Vector2D) As Vector2D
+            Dim x As Single = x + vector.X
+            Dim y As Single = y + vector.Y
+            Return New Vector2D(x, y)
+        End Function
+
+        Public Function Subtract(ByVal vector As Vector2D) As Vector2D
+            Dim x As Single = x - vector.X
+            Dim y As Single = y - vector.Y
+            Return New Vector2D(x, y)
+        End Function
+
+        Public Shared Operator -(value1 As Vector2D, value2 As Vector2D) As Vector2D
+            Return value1.Subtract(value2)
+        End Operator
+
+        Public Shared Operator +(value1 As Vector2D, value2 As Vector2D) As Vector2D
+            Return value1.Add(value2)
+        End Operator
+
+        Public Shared Operator =(value1 As Vector2D, value2 As Vector2D) As Boolean
+            Return value1.X = value2.X AndAlso value1.Y = value2.Y
+        End Operator
+
+        Public Shared Operator <>(value1 As Vector2D, value2 As Vector2D) As Boolean
+            Return value1.X <> value2.X OrElse value1.Y <> value2.Y
+        End Operator
+
+        Public Overrides Function Equals(ByVal obj As Object) As Boolean
+            If TypeOf obj Is Vector2D Then
+                Dim o = CType(obj, Vector2D)
+
+                If Me.X = o.X AndAlso Me.Y = o.Y Then
+                    Return True
+                Else
+                    Return False
+                End If
+            Else
+                Return MyBase.Equals(obj)
+            End If
+        End Function
+
+        Public Overrides Function GetHashCode() As Integer
+            Return MyBase.GetHashCode()
+        End Function
+
+    End Class
+
+End Namespace
