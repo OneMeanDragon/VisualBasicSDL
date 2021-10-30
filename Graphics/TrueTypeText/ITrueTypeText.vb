501b58b8007dff3363d8385b93e73a696f23f113
OneMeanDragon <dragonkings@gmail.com>
2021-10-30 12:37:29 -0400

    Add files via upload

---
+++ Graphics/TrueTypeText/ITrueTypeText.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+
+Namespace Graphics
+    Interface ITrueTypeText
+        Inherits IDisposable
+
+        Property Text As String
+        Property Font As IFont
+        Property Color As SDLColor
+        Property Texture As ITexture
+        Property OutlineSize As Integer
+        Property IsWrapped As Boolean
+        Property WrapLength As Integer
+        Sub SetOutlineSize(ByVal outlineSize As Integer)
+        Sub UpdateText(ByVal text As String)
+        Sub UpdateText(ByVal text As String, ByVal wrapLength As Integer)
+    End Interface
+End Namespace
