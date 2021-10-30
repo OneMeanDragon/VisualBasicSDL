3a7b11f419b36c3bb7d14fb93e47d35cfc99c8e0
OneMeanDragon <dragonkings@gmail.com>
2021-10-30 12:35:48 -0400

    Add files via upload

---
+++ Graphics/Renderer/IRenderer.vb
@@ -0,0 +1,23 @@
+﻿Imports System
+Imports System.Collections.Generic
+
+Namespace Graphics
+    Interface IRenderer
+        Inherits IDisposable
+
+        Property Window As IWindow
+        Property Index As Integer
+        Property Flags As IEnumerable(Of RendererFlags)
+        Property Handle As IntPtr
+        Sub ClearScreen()
+        Sub RenderPresent()
+        Sub RenderTexture(ByVal texture As ITexture, ByVal positionX As Single, ByVal positionY As Single, ByVal sourceWidth As Integer, ByVal sourceHeight As Integer, ByVal angle As Double, ByVal center As Vector2D)
+        Sub RenderTexture(ByVal texture As ITexture, ByVal positionX As Single, ByVal positionY As Single, ByVal sourceWidth As Integer, ByVal sourceHeight As Integer)
+        Sub RenderTexture(ByVal texture As ITexture, ByVal positionX As Single, ByVal positionY As Single, ByVal source As Rectangle)
+        Sub ResetRenderTarget()
+        Sub SetBlendMode(ByVal blendMode As BlendMode)
+        Sub SetDrawColor(ByVal r As Byte, ByVal g As Byte, ByVal b As Byte, ByVal a As Byte)
+        Sub SetRenderLogicalSize(ByVal width As Integer, ByVal height As Integer)
+        Sub SetRenderTarget(ByVal renderTarget As ITexture)
+    End Interface
+End Namespace
---
+++ Graphics/Renderer/RendererFlags.vb
@@ -0,0 +1,11 @@
+﻿Imports SDL2
+
+Namespace Graphics
+    <Flags>
+    Public Enum RendererFlags As UInteger
+        None = 0
+        RendererAccelerated = SDL.SDL_RendererFlags.SDL_RENDERER_ACCELERATED
+        RendererPresentVSync = SDL.SDL_RendererFlags.SDL_RENDERER_PRESENTVSYNC
+        SupportRenderTargets = SDL.SDL_RendererFlags.SDL_RENDERER_TARGETTEXTURE
+    End Enum
+End Namespace
